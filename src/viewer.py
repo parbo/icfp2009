b307792501ea4bc4b116e7cc58ed1d28d9b11f62
dang <dang@localhost>
2009-06-27 17:13:11 +0200

    Added output from GUI.

--- src/viewer.py
+++ src/viewer.py
@@ -1,4 +1,5 @@
 import sys
+import math
 import os.path
 import wx
 
@@ -16,10 +17,12 @@ ID_STEP_BTN = 104
 ID_RUN_BTN = 105
 ID_ZOOM_IN_BTN = 106
 ID_ZOOM_OUT_BTN = 107
+ID_OUTFILE_SELECT_BTN = 108
+ID_WRITE_BTN = 109
 ID_TEST_BTN = 999
 
 class Viewer(wx.Frame):
-    def __init__(self, controller='', problem='', conf=1001):
+    def __init__(self, controller='', problem='', conf=1001, outfile='out.bin'):
         wx.Frame.__init__(self, None, -1, TITLE, size = (800, 600), style = wx.DEFAULT_FRAME_STYLE)
         # Frame initializations.
         self.SetBackgroundColour(wx.LIGHT_GREY)
@@ -31,7 +34,12 @@ class Viewer(wx.Frame):
         self.problemInput = wx.TextCtrl(self, -1, problem)
         self.problemLabel = wx.StaticText(self, -1, 'Problem file:')
         self.problemSelectBtn = wx.Button(self, ID_PROBLEM_SELECT_BTN, 'Select')
+        self.outfileInput = wx.TextCtrl(self, -1, outfile)
+        self.outfileLabel = wx.StaticText(self, -1, 'Output file:')
+        self.outfileSelectBtn = wx.Button(self, ID_OUTFILE_SELECT_BTN, 'Select')
         self.configInput = wx.SpinCtrl(self, -1, str(conf), min=1000, max=9999, initial=conf)
+        self.writeBtn = wx.Button(self, ID_WRITE_BTN, 'Write Result')
+        self.writeBtn.Disable()
         self.loadBtn = wx.Button(self, ID_LOAD_BTN, 'Load')
         self.stepBtn = wx.Button(self, ID_STEP_BTN, 'Step')
         self.stepInput = wx.SpinCtrl(self, -1, '1', min=1, max=3000000, initial=1)
@@ -43,10 +51,12 @@ class Viewer(wx.Frame):
         self.mainSizer = wx.BoxSizer(wx.VERTICAL)
         self.controlFileSizer = wx.BoxSizer(wx.HORIZONTAL)
         self.problemFileSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.outFileSizer = wx.BoxSizer(wx.HORIZONTAL)
         self.canvasSizer = wx.BoxSizer(wx.HORIZONTAL)
         self.commandSizer = wx.BoxSizer(wx.VERTICAL)
         self.mainSizer.Add(self.controlFileSizer, 0, flag = wx.EXPAND)
         self.mainSizer.Add(self.problemFileSizer, 0, flag = wx.EXPAND)
+        self.mainSizer.Add(self.outFileSizer, 0, flag = wx.EXPAND)
         self.mainSizer.Add(self.canvasSizer, 1, flag = wx.EXPAND)
         self.controlFileSizer.Add(self.controlLabel, 1, flag = wx.ALIGN_LEFT)
         self.controlFileSizer.Add(self.controlInput, 7, flag = wx.EXPAND)
@@ -54,8 +64,12 @@ class Viewer(wx.Frame):
         self.problemFileSizer.Add(self.problemLabel, 1, flag = wx.ALIGN_LEFT)
         self.problemFileSizer.Add(self.problemInput, 7, flag = wx.EXPAND)
         self.problemFileSizer.Add(self.problemSelectBtn, 0, flag = wx.ALIGN_RIGHT)
+        self.outFileSizer.Add(self.outfileLabel, 1, flag = wx.ALIGN_LEFT)
+        self.outFileSizer.Add(self.outfileInput, 7, flag = wx.EXPAND)
+        self.outFileSizer.Add(self.outfileSelectBtn, 0, flag = wx.ALIGN_RIGHT)
         self.canvasSizer.Add(self.commandSizer, 0, flag = wx.EXPAND)
         self.canvasSizer.Add(self.canvas, 1, flag = wx.EXPAND)
+        self.commandSizer.Add(self.writeBtn, 0, flag = wx.EXPAND)
         self.commandSizer.Add(self.loadBtn, 0, flag = wx.EXPAND)
         self.commandSizer.Add(self.configInput, 0, flag = wx.EXPAND)
         self.commandSizer.Add(self.stepBtn, 0, flag = wx.EXPAND)
@@ -65,11 +79,13 @@ class Viewer(wx.Frame):
         self.commandSizer.Add(self.zoomOutBtn, 0, flag = wx.EXPAND)
         self.SetSizer(self.mainSizer)
         # Status bar definitions.
-        bar = self.CreateStatusBar(7)
-        bar.SetStatusWidths([-2, -2, -2, -2, -2, -2, -3])
+        bar = self.CreateStatusBar(8)
+        bar.SetStatusWidths([-2, -2, -2, -2, -2, -2, -2, -3])
         # Event initializations.
         self.Bind(wx.EVT_BUTTON, self.OnControlSelectBtn, id = ID_CONTROL_SELECT_BTN)
         self.Bind(wx.EVT_BUTTON, self.OnProblemSelectBtn, id = ID_PROBLEM_SELECT_BTN)
+        self.Bind(wx.EVT_BUTTON, self.OnOutfileSelectBtn, id = ID_OUTFILE_SELECT_BTN)
+        self.Bind(wx.EVT_BUTTON, self.OnWriteBtn, id = ID_WRITE_BTN)
         self.Bind(wx.EVT_BUTTON, self.OnLoadBtn, id = ID_LOAD_BTN)
         self.Bind(wx.EVT_BUTTON, self.OnStepBtn, id = ID_STEP_BTN)
         self.Bind(wx.EVT_BUTTON, self.OnRunBtn, id = ID_RUN_BTN)
@@ -95,8 +111,24 @@ class Viewer(wx.Frame):
             self.problemInput.SetValue(dlg.GetPath())
         dlg.Destroy()
         
+    def OnOutfileSelectBtn(self, event):
+        print 'OnOutfileSelectBtn'
+        dlg = wx.FileDialog(self, 'Select output file', wildcard = 'Output file (*.*)|*.*', style = wx.OPEN | wx.CHANGE_DIR)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.outfileInput.SetValue(dlg.GetPath())
+        dlg.Destroy()
+        
+    def OnWriteBtn(self, event):
+        print 'OnWriteBtn'
+        filename = self.outfileInput.GetValue()
+        if filename != '':
+            f = open(filename, 'wb')
+            f.write(str(self.sim.submission))
+            f.close()
+        
     def OnLoadBtn(self, event):
         print 'OnLoadBtn'
+        self.writeBtn.Disable()
         controller_path = self.controlInput.GetValue()
         problem = self.problemInput.GetValue()
         ctrldirpath = os.path.dirname(controller_path)
@@ -112,19 +144,12 @@ class Viewer(wx.Frame):
         
     def OnStepBtn(self, event):
         print 'OnStepBtn'
-        states = self.sim.step(int(self.stepInput.GetValue()))
-        dc = wx.ClientDC(self.canvas)
-        for state in states:
-            self.canvas.PointW(dc, state.sx, state.sy)
-        self.UpdateStatusBar()
+        self.Run(int(self.stepInput.GetValue()))
         
     def OnRunBtn(self, event):
         print 'OnRunBtn'
-        while not self.sim.completed > 0:
-            self.sim.step()
-            dc = wx.ClientDC(self.canvas)
-            self.canvas.PointW(dc, self.sim.state.sx, self.sim.state.sy)
-            self.UpdateStatusBar()
+        while not self.sim.completed:
+            self.Run(int(self.stepInput.GetValue()))
             
     def OnZoomInBtn(self, event):
         print 'OnZoomInBtn'
@@ -134,14 +159,25 @@ class Viewer(wx.Frame):
         print 'OnZoomOutBtn'
         self.canvas.Zoom(1.5)
         
+    def Run(self, steps):
+        states = self.sim.step(steps)
+        dc = wx.ClientDC(self.canvas)
+        for state in states:
+            self.canvas.PointW(dc, state.sx, state.sy)
+        self.UpdateStatusBar()
+        if self.sim.completed:
+            self.writeBtn.Enable()
+        
     def UpdateStatusBar(self):
         bar = self.GetStatusBar()
         try:
             sx = 'sx: %.3e' % self.sim.state.sx
             sy = 'sy: %.3e' % self.sim.state.sy
+            r = 'r: %.3e' % math.sqrt(self.sim.state.sx ** 2 + self.sim.state.sy ** 2)
         except TypeError:
             sx = 'sx: -'
             sy = 'sy: -'
+            r = 'r: -'
         try:
             vx = 'vx: %.3e' % self.sim.state.vx
             vy = 'vy: %.3e' % self.sim.state.vy
@@ -154,11 +190,12 @@ class Viewer(wx.Frame):
             fuel = 'fuel: -'
         bar.SetStatusText('t: %d' % self.sim.time, 0)
         bar.SetStatusText('s: %.1f' % self.sim.state.score, 1)
-        bar.SetStatusText(sx, 2)
-        bar.SetStatusText(sy, 3)
-        bar.SetStatusText(vx, 4)
-        bar.SetStatusText(vy, 5)
-        bar.SetStatusText(fuel, 6)
+        bar.SetStatusText(r, 2)
+        bar.SetStatusText(sx, 3)
+        bar.SetStatusText(sy, 4)
+        bar.SetStatusText(vx, 5)
+        bar.SetStatusText(vy, 6)
+        bar.SetStatusText(fuel, 7)
         
 class Canvas(wx.Panel):
     def __init__(self, parent):
